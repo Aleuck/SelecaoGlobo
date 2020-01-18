48ac37e8a2a3a708e7c41f856d46d1a720f9b65f
A. Leuck <aleuck@aleuck.dev>
2020-01-18 11:22:41 -0300

    fe: update theme

--- fe/themes/default/index.js
+++ fe/themes/default/index.js
@@ -1,2 +1,19 @@
-import styles from './default.scss';
+import layout from './layout.scss';
+import button from './button.scss';
+import selectable from './selectable.scss';
+import modal from './modal.scss';
+import modalHeader from './modal-header.scss';
+import modalFooter from './modal-footer.scss';
+import participant from './participant.scss';
+
+const styles = Object.assign({},
+  layout,
+  button,
+  selectable,
+  modal,
+  modalHeader,
+  modalFooter,
+  participant,
+);
+
 export default styles;
