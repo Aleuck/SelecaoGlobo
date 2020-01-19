5310cede6d969d1a416b050004b9c8a146741c3d
A. Leuck <aleuck@aleuck.dev>
2020-01-19 13:07:01 -0300

    fe: add propType model for participants

---
+++ fe/models/participant.js
@@ -0,0 +1,12 @@
+import PropTypes from 'prop-types';
+
+/** Participant propTypes model */
+const propTypes = {
+  id: PropTypes.string,
+  name: PropTypes.string,
+  image: PropTypes.string,
+  callNumber: PropTypes.string,
+  smsNumber: PropTypes.string,
+};
+
+export { propTypes };
