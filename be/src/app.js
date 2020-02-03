d7e92d2498c56118b113b6642e3a78e79d78e190
A. Leuck <578126+Aleuck@users.noreply.github.com>
2020-02-03 00:04:56 -0300

    be: create default initial data

--- be/src/app.js
+++ be/src/app.js
@@ -54,13 +54,75 @@ app.use(express.errorHandler({ logger }));
 
 app.hooks(appHooks);
 
-// Create default user
-app.service('users').create(app.get('defaultUser'))
-  .then(() => {
-    logger.info('Default user created.');
-  })
-  .catch(() => {
-    // Default user already exists
-  });
+app.on('sequelizeSync', () => {
+  // Create default user
+  app.service('users').create(app.get('defaultUser'))
+    .then(() => {
+      logger.info('Default user created.');
+
+      Promise.resolve()
+        .then(() => {
+          logger.info('Creating initial data:');
+          logger.info(' - season');
+          return app.service('seasons').create({
+            title: '2020',
+            startsAt: new Date('2020-01-21T12:00:00.000-03:00').toISOString(),
+            endsAt: new Date('2020-12-18T23:00:00.000-03:00').toISOString(),
+          });
+        })
+        .then(season => {
+          logger.info(' - participants');
+          return Promise.all([
+            app.service('participants').create({
+              seasonId: season.id,
+              name: 'Participante 1',
+              image: '2887b6af8175a6e20880c0dde8f1c9ae9cf0cbb9b79b7ec1f5910313daa8693d.png',
+            }),
+            app.service('participants').create({
+              seasonId: season.id,
+              name: 'Participante 2',
+              image: '4bb3630816c607757a12af214da0c8aa417766ccce3d5b6255ea9a18f853144a.png',
+            }),
+          ])
+            .then(participants => {
+              logger.info(' - wall');
+              const startsAt = new Date();
+              startsAt.setUTCHours(23, 0, 0, 0);
+              const endsAt = new Date();
+              endsAt.setUTCHours(3, 0, 0, 0);
+              endsAt.setUTCDate(startsAt.getUTCDate() + 1);
+
+              return app.service('walls').create({
+                title: 'Paredão 1',
+                seasonId: season.id,
+                startsAt: startsAt.toISOString(),
+                endsAt: endsAt.toISOString(),
+              }).then(wall => {
+                logger.info(' - wall participants');
+                return Promise.all([
+                  app.service('walls-participants').create({
+                    participantId: participants[0].id,
+                    wallId: wall.id,
+                    callNumber: '0800-123-01',
+                    smsNumber: '8001',
+                  }),
+                  app.service('walls-participants').create({
+                    participantId: participants[1].id,
+                    wallId: wall.id,
+                    callNumber: '0800-123-02',
+                    smsNumber: '8002',
+                  }),
+                ]);
+              });
+            });
+        })
+        .catch(reason => {
+          console.log('Failed to create initial data:', reason);
+        });
+    })
+    .catch(() => {
+      // Default user already exists
+    });
+});
 
 module.exports = app;
